d1e815764eafda2047e70e6d9d6fc80758f86074
SophisticatedPrimate <admin@sophisticatedprimate.com>
2022-09-18 10:12:26 -0500

    Add files via upload

---
+++ long_addition_with_decimals_1.py
@@ -0,0 +1,156 @@
+"""
+Check out the official python docs to learn more about python programming!
+https://docs.python.org/3/
+
+Summations are found in python by using the addition operator (+), in which two addends on either side are summed.
+Basic usage is as follows:
+    print(5 + 3) 
+    sum = addendA + addendB
+    difference = positive + (-negative)
+
+Decimal points are represented by floating point values in programming (IEEE Standard 754).
+Calculating values by hand and using a calculator are essentially different operations that yield effectively similar results.
+When calculating by hand, a definite answer is always provided because you are calculating using the defition of operations and values.
+Since memory within a computer is finite, bitwise operations are used to perform math, and precision and magnitude are factors that must be considered.
+Pretty much, you are only able to be accurate within a specified range of magnitude and degree of precision (in a direct trade off).
+Learn more about floating point numbers at:
+    https://floating-point-gui.de/formats/fp/
+    https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
+
+The other operations used in this program are print(), and format().
+print() is used to display the results of the operation to the console.
+format() is makes it look nice.
+Learn more at: https://docs.python.org/3/tutorial/inputoutput.html
+
+To run the program: 
+    1. Open a terminal on a computer with python 3 installed (works with v2 as well)
+    2. Navigate to a directory containing this file (long_addition_with_decimals_1.py)
+    3. Run the program with the following command: python3 long_addition_with_decimals_1.py
+    4. A print out of the sums should appear after the cursor
+
+If error:
+    a. make sure python is installed (run: python3 --version)
+    b. make sure you have the file installed and in the current directory
+    c. if you are still having issues or see any general issues in the programming please report using the contact information at end of file.
+"""
+
+# Problem 1  
+sum = 36.56 + 88.42
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(36.56,88.42,sum))
+
+# Problem 2  
+sum = 38.06 + 42.019
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(38.06,42.019,sum))
+
+# Problem 3  
+sum = 88.416 + 87.23
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(88.416,87.23,sum))
+
+# Problem 4  
+sum = 114.416 + 66.23
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(114.416,66.23,sum))
+
+# Problem 5  
+sum = 7 + 0.8788
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(7,0.8788,sum))
+
+# Problem 6  
+sum = 57.71 + 49.83
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(57.71,49.83,sum))
+
+# Problem 7  
+sum = 77.715 + 20.702
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(77.715,20.702,sum))
+
+# Problem 8  
+sum = 39.613 + 16.519
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(39.613,16.519,sum))
+
+# Problem 9  
+sum = 63.62 + 25.52
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(63.62,25.52,sum))
+
+# Problem 10 
+sum = 121.02 + 19.71
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(121.02,19.71,sum))
+
+# Problem 11 
+sum = 114.011 + 36.08
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(114.011,36.08,sum))
+
+# Problem 12 
+sum = 0.8484 + 0.267
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(0.8484,0.267,sum))
+
+# Problem 13 
+sum = 89.55 + 69.52
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(89.55,69.52,sum))
+
+# Problem 14 
+sum = 70.52 + 48.22
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(70.52,48.22,sum))
+
+# Problem 15 
+sum = 47.716 + 4.914
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(47.716,4.914,sum))
+
+# Problem 16 
+sum = 712.515 + 38.91
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(712.515,38.91,sum))
+
+# Problem 17 
+sum = 0.7978 + 95.5
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(0.7978,95.5,sum))
+
+# Problem 18 
+sum = 412.714 + 43.011
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(412.714,43.011,sum))
+
+# Problem 19 
+sum = 43.513 + 10.315
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(43.513,10.315,sum))
+
+# Problem 20 
+sum = 112.85 + 92.215
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(112.85,92.215,sum))
+
+# Problem 21 
+sum = 612.713 + 33.316
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(612.713,33.316,sum))
+
+# Problem 22 
+sum = 85.26 + 59.38
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(85.26,59.38,sum))
+
+# Problem 23 
+sum = 810.14 + 610.53
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(810.14,610.53,sum))
+
+# Problem 24 
+sum = 66.917 + 72.317
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(66.917,72.317,sum))
+
+# Problem 25 
+sum = 39.91 + 11.818
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(39.91,11.818,sum))
+
+# Problem 26 
+sum = 6.55 + 73.815
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(6.55,73.815,sum))
+
+# Problem 27 
+sum = 168.5 + 1.444
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(168.5,1.444,sum))
+
+# Problem 28 
+sum = 49.07 + 1.118
+print("{0:<8} + {1:<8} = {2:<9.4f}".format(49.07,1.118,sum))
+
+"""
+Contact information
+
+General Queries: admin@sophisticatedprimate.com
+Report errors, bugs, and problems at:
+    email:  bug@sophisticatedprimate.com
+    github: https://github.com/sophisticatedprimate/long_addition_with_decimals_1/issues
+"""
